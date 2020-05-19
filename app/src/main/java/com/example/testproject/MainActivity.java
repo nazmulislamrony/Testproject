55cf84c201b839cc7e30f97fc681b8e170941318
nazmulisrony <nazmulisrony@gmail.com>
2020-05-19 23:03:22 +0600

    hello world

---
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
